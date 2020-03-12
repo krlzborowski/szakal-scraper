3adf4dd23fb40e30850bf427c7b62fccf86b0b5a
Karol <krlzborowski@gmail.com>
2020-03-12 20:01:10 +0100

    Add opening taken companies

--- scraper.py
+++ scraper.py
@@ -1,2 +1,19 @@
+from bs4 import BeautifulSoup
+
+URL_TAKEN_COMPANIES = 'http://baza-firm.lysik.pl/bf/companies/taken/1?sort%5Bname%5D=&sort%5Border%5D=&filter%5Bname' \
+                      '%5D=&filter%5Bgroup%5D=1&filter%5Buser%5D=&filter%5Bstatus%5D=&filter%5Bfinished%5D=&filter' \
+                      '%5Blimit%5D=ALL&submit=Filtruj '
+
+
 class Scraper:
-    pass
+
+    def __init__(self, browser):
+        self.statuses = {}
+        self.br = browser
+
+    def count_statuses(self):
+        r = self.br.open(URL_TAKEN_COMPANIES)
+        soup = BeautifulSoup(r, 'html5lib')
+        page_code = soup.prettify()
+        print(page_code)
+
